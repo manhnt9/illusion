663c8d1b036b62910c0cfb08bc80b7677562ec0d
zer0x <zer0.x@outlook.com>
2016-10-21 15:50:10 +0700

    ILDelayedOperation: fix timer code

--- benchmark/ildelayedoperation.cpp
+++ benchmark/ildelayedoperation.cpp
@@ -16,17 +16,12 @@ ILDelayedOperation::ILDelayedOperation(ILOperation* op)
 void ILDelayedOperation::run(uint64_t delay) {
     auto self(shared_from_this());
 
+    timer_.expires_from_now(std::chrono::milliseconds(delay));
     timer_.async_wait([this, self] (const std::error_code& e) {
         if (!e)
             this->op_->run();
-
-        timer_.async_wait([this, self] (const std::error_code& e) {
-            if (!e)
-                this->op_->run();
-        });
     });
 
-    timer_.expires_from_now(std::chrono::milliseconds(delay));
 }
 
 } // namespace il
