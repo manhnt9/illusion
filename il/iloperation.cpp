58fece1e2f2897a6f98f4443a41d34ae49d13ee0
zer0x <zer0.x@outlook.com>
2016-11-28 17:02:26 +0700

    Minor style fix

--- il/iloperation.cpp
+++ il/iloperation.cpp
@@ -3,11 +3,11 @@
 namespace il {
 
 ILOperation::ILOperation()
-  : QObject(nullptr),
-    id_(0),
-    state_(ILState::CREATED),
-    startTime_(0),
-    finishTime_(0)
+  :  QObject(nullptr),
+     id_(0),
+     state_(ILState::CREATED),
+     startTime_(0),
+     finishTime_(0)
 {
 
 }
