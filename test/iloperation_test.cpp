1c6621dc4d4cd26ce390dde719394bf962e1c2a1
zer0x <zer0.x@outlook.com>
2016-12-10 10:22:46 +0700

    Add unit test for ILOperation

---
+++ test/iloperation_test.cpp
@@ -0,0 +1,49 @@
+#include <catch.hpp>
+#include <il/operation/iloperation.hpp>
+#include <il/operation/iloperation_base.hpp>
+#include <il/ilstate.hpp>
+#include <thread>
+#include <chrono>
+
+using namespace il;
+using namespace std::chrono_literals;
+
+class ILOperationDummy : public ILOperationBase {
+public:
+  virtual void run() { start(); }
+  virtual void abort() { stop(); }
+};
+
+class ILOperationTestFixture {
+public:
+  ILOperationTestFixture() { operation_ = std::make_unique<ILOperationDummy>(); }
+
+protected:
+  std::unique_ptr<ILOperation> operation_;
+};
+
+TEST_CASE_METHOD(ILOperationTestFixture, "Create ILOperation", "[create_ilop]") {
+  REQUIRE(operation_->state() == ilstate::CREATED);
+}
+
+TEST_CASE_METHOD(ILOperationTestFixture, "Run ILOperation", "[run_ilop]") {
+  operation_->run();
+  REQUIRE(operation_->isRunning() == true);
+  REQUIRE(operation_->state() == ilstate::RUNNING);
+}
+
+TEST_CASE_METHOD(ILOperationTestFixture, "Abort ILOperation", "[abort_ilop]") {
+  operation_->run();
+  operation_->abort();
+  REQUIRE(operation_->isRunning() == false);
+  REQUIRE(operation_->state() == ilstate::STOPPED);
+}
+
+TEST_CASE_METHOD(ILOperationTestFixture, "ILOperation timer", "[ilop_timer]") {
+  operation_->run();
+  std::this_thread::sleep_for(100ms);
+  operation_->abort();
+
+  // allow less than 6ms difference
+  REQUIRE(operation_->duration() == Approx(100).epsilon(0.01));
+}
